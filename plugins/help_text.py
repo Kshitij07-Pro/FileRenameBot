4aa5b8386ec18da3f67e9f7e1232af53b27cc36e
Kshitij07-Pro <85276195+Kshitij07-Pro@users.noreply.github.com>
2021-07-16 19:48:25 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -51,7 +51,7 @@ async def help_user(bot, update):
             await update.reply_text(
                 text="**Please Join My Update Channel Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="Join My Chat Group Onii Chan", url=f"https://t.me/{update_channel}")]
               ])
             )
             return
@@ -90,7 +90,7 @@ async def start_me(bot, update):
             await update.reply_text(
                 text="**Please Join My Update Channel Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="Join My Chat Group Onii Chan", url=f"https://t.me/{update_channel}")]
               ])
             )
             return
@@ -102,12 +102,8 @@ async def start_me(bot, update):
                         InlineKeyboardButton("Help", callback_data = "ghelp")
                 ],
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
-                ],
-                [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Anime Group', url='https://t.me/Anime_Discussion_requests'),
+                    InlineKeyboardButton('Editied By', url='https://t.me/xitij2049')
                 ]
             ]
         ),
